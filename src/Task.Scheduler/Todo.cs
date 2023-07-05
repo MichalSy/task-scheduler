5fdbf9efaec9c29800d1fe44204a4e98d3ce95de
Michal Sytko <michal.sytko@gmail.com>
2023-07-05 21:03:39 +0200

    add deployment

--- src/Task.Scheduler/Todo.cs
+++ src/Task.Scheduler/Todo.cs
@@ -15,7 +15,7 @@ internal static class TodoGenerator
 {
     private static readonly (string[] Prefixes, string[] Suffixes)[] _parts = new[]
         {
-            (new[] { "Walk the", "Feed the" }, new[] { "dog", "cat", "goat" }),
+            (new[] { "Walk th2e", "Feed the" }, new[] { "dog", "cat", "goat" }),
             (new[] { "Do the", "Put away the" }, new[] { "groceries2", "dishes", "laundry" }),
             (new[] { "Clean the" }, new[] { "bathroom", "pool", "blinds", "car" })
         };
