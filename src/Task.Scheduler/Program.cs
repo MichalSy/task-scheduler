9e907072c7e6d416da72bbdbe4baf5d8d4e0a37e
Michal Sytko <michal.sytko@gmail.com>
2023-07-17 22:19:33 +0200

    test

--- src/Task.Scheduler/Program.cs
+++ src/Task.Scheduler/Program.cs
@@ -1,10 +1,20 @@
 using Task.Scheduler;
-using FastEndpoints.Swagger;
+//using FastEndpoints.Swagger;
+using Neo4j.Driver;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder();
 
-builder.Services.AddFastEndpoints();
-builder.Services.SwaggerDocument();
+//builder.Services.AddFastEndpoints();
+//builder.Services.SwaggerDocument();
+
+
+builder.Services.AddSingleton(
+    GraphDatabase.Driver(
+        builder.Configuration["NEO4J_SERVER"], 
+        AuthTokens.Basic(builder.Configuration["NEO4J_USER"], builder.Configuration["NEO4J_PASSWORD"])
+    )
+);
 
 var app = builder.Build();
 
@@ -22,7 +32,18 @@ app.MapGet("/test", () =>
     return Environment.GetEnvironmentVariables();
 });
 
+app.MapGet("/conf", (IConfiguration conf) =>
+{
+    return conf.GetValue<string>("NEO4J_USER");
+});
 
-app.UseFastEndpoints();
-app.UseSwaggerGen();
+app.MapGet("/test2", async (IDriver driver) =>
+{
+    await driver.VerifyConnectivityAsync();
+    var t = await driver.ExecutableQuery("CREATE (n) RETURN n").ExecuteAsync();
+    return "Hello";
+});
+
+//app.UseFastEndpoints();
+//app.UseSwaggerGen();
 app.Run();
